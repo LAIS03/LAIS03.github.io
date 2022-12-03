ab2a67d400d4f45b7c8ede6e75d7f0f038582f0f
LAIS03 <119751335+LAIS03@users.noreply.github.com>
2022-12-03 12:47:30 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-alert("Holii, espera un poco la página esta cargando");
+//alert("Bienvenidos a mi pagina");
 
-//Código para la animación
-
-let app = document.getElementByld('typewriter');
+//Este código es para la animación
+let app = document.getElementById('typewriter');
 
 let typewriter = new Typewriter(app, {
-    loop: true, 
-    delay: 75,   
-});    
-    
+    loop: true,
+    delay: 75,
+});
+
 typewriter
 .pauseFor(2500)
-.typeString('Desarrollo sitios web y escribo artículos sobre programación')
+.typeString('estudio en la carrera de Ingeniería en Animación Digital y Efectos Visuales')
 .pauseFor(200)
 .deleteChars(10)
 .start();
